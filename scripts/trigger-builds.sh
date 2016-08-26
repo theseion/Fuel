7258f6edeb2d9e2108fd1ba1b35f647352375304
Max Leske <maxleske@gmail.com>
2016-08-26 11:37:14 +0200

    also trigger Appveyor build; renamed 'trigger-travis.sh' to 'trigger-builds.sh'

--- scripts/trigger-builds.sh
+++ scripts/trigger-builds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-body='{
+BODY='{
 "request": {
   "message":"Build triggered by Jenkins build on ci.inria.fr",
   "branch":"master"
@@ -11,7 +11,19 @@ curl -m 5 -s -X POST \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token LOvjbefQWMRVTnyibGNl4A" \
-  -d "$body" \
+  -d "${BODY}" \
   https://api.travis-ci.org/repo/theseion%2FFuel/requests
+
+BODY='{
+  "accountName": "theseion",
+  "projectSlug": "fuel"
+}'
+
+curl -m 5 -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Authorization: Bearer 7vsywh57m1iaibnb28r1" \
+  -d "${BODY}" \
+  https://ci.appveyor.com/api/builds
 
 exit 0
